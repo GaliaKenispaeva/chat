e6260cdac0afe930bdab5a339452b47e1275e872
Galia Kenispaeva <galia.kenispaeva@gmail.com>
2020-01-11 20:01:37 +0600

    add sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-
+config.action_mailer.default_url_options = { :host => 'desolate-beyond-67025.herokuapp.com'}
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.default_url_options = { :host => 'desolate-beyond-67025.herokuapp.com' }

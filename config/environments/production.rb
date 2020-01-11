ff6a3321634129812f410ce58b93c087485b3cad
Galia Kenispaeva <galia.kenispaeva@gmail.com>
2020-01-11 20:06:28 +0600

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      'galia.kenispaeva@gmail.com',
+    password:       'JuzikKeni',
+    domain:         'localhost:3000',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
